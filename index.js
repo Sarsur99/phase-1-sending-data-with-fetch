114c3fb2f5b8099c12875955d2d3ba248227b093
Soren <sorenisme@gmail.com>
2022-09-26 10:15:49 -0400

    commit tree 181ae81df885f5edc4ea4872f026cc274090d140

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+// Add your code
+
+// const inputs = {
+//     name: "",
+//     email: "",
+// };
+// const configurationObject  = {
+//     method: 'POST',
+
+//     headers:{
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(inputs)
+// };
+
+function submitData(name, email){
+    // console.log(configurationObject)
+    fetch("http://localhost:3000/users", {
+        method: 'POST',
+    
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+    .then(function(response){
+    return response.json();
+})
+    .then(function(object) {
+    let theBod = getElementsByClassName("body")
+    console.log(theBod)
+})
+
+}
+submitData();
